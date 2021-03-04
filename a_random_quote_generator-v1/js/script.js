52e8eb9efb7355438634f98e28f24f1e99e733f0
Karina Abad <karinajl19@gmail.com>
2021-03-03 17:35:22 -0800

    updated array and finished random quote function

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -10,29 +10,58 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-const quotes = [
-  ['You miss 100% of the shots you don\'t take.', 'Wayne Gretzky'],
-  ['Whether you think you can or you think you can\'t, you\'re right.', 'Henry Ford'],
-  ['I have learned over the years that when one\'s mind is made up, this diminishes fear.', 'Rosa Parks'],
-  ['I alone cannot change the world, but I can cast a stone across the water to create many ripples.', 'Mother Teresa'],
-  ['Your time is limited, so don\'t waste it living someone else\'s life. Don\'t be trapped by dogma – which is living with the results of other people\'s thinking.', 'Steve Jobs'],
-  ['If life were predictable it would cease to be life, and be without flavor.', 'Eleanor Roosevelt'],
-  ['If you look at what you have in life, you\'ll always have more. If you look at what you don\'t have in life, you\'ll never have enough.', 'Oprah Winfrey'],
-  ['Mama, mama, mama, mama', 'Javi Lopez', '2021'],
-  ['It\'s the climb', 'Miley Cyrus', 'Hannah Montana: The Movie']
+let quotes = [
+  {
+    quote: 'You miss 100% of the shots you don\'t take.', 
+    source: 'Wayne Gretzky'
+  },
+  {
+    quote: 'Whether you think you can or you think you can\'t, you\'re right.',
+    source: 'Henry Ford'
+  },
+  {
+    quote: 'I have learned over the years that when one\'s mind is made up, this diminishes fear.',
+    source: 'Rosa Parks'
+  },
+  {
+    quote: 'I alone cannot change the world, but I can cast a stone across the water to create many ripples.', 
+    source: 'Mother Teresa'
+  },
+  {
+    quote: 'Your time is limited, so don\'t waste it living someone else\'s life. Don\'t be trapped by dogma – which is living with the results of other people\'s thinking.',
+    source: 'Steve Jobs'
+  },
+  {
+    quote: 'If life were predictable it would cease to be life, and be without flavor.', 
+    source: 'Eleanor Roosevelt'
+  },
+  {
+    quote: 'If you look at what you have in life, you\'ll always have more. If you look at what you don\'t have in life, you\'ll never have enough.',
+    source: 'Oprah Winfrey'
+  },
+  {
+    quote: 'Mama, mama, mama, mama', 
+    source: 'Javi Lopez',
+    year: '2021'
+  },
+  {
+    quote: 'It\'s the climb', 
+    source: 'Miley Cyrus',
+    citation: 'Hannah Montana: The Movie'
+  }
 ]
 
 
 /***
  * `getRandomQuote` function
 ***/
-const getRandomQuote = function(quotes) {
-  let randomQuoteIndex = Math.floor(Math.random() * 9);
-
-  return quotes[randomQuoteIndex];
+function getRandomQuote(arr) {
+  let randomQuoteIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomQuoteIndex];
 }
 
-
+console.log(quotes);
+console.log(getRandomQuote(quotes));
 /***
  * `printQuote` function
 ***/
